e36d95f9542cc6ea69289ce534bd1d11bb6d965d
featitx <siddabdul7@gmail.com>
2024-01-24 17:41:17 +0530

    tailwind added

--- Stopwatch/src/App.jsx
+++ Stopwatch/src/App.jsx
@@ -1,56 +1,62 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react'
+import React, { useEffect } from "react";
+import { useState } from "react";
 
 const App = () => {
-
   let [time, setTime] = useState(0);
-  const [running , setRunning] = useState(false)
+  const [running, setRunning] = useState(false);
 
-
-  useEffect(    
-    ()=>{
-      let intervalId ;
-      if(running){
-        intervalId = setInterval(()=>{
-          setTime((prevTime) => prevTime + 10)
-        },10) 
-      }
-
-      else if(!running){
-        clearInterval(intervalId)
-      }
-      return () =>{clearInterval(intervalId)}
-
-    }, [running]
-  )
-
-  
+  useEffect(() => {
+    let intervalId;
+    if (running) {
+      intervalId = setInterval(() => {
+        setTime(prevTime => prevTime + 10);
+      }, 10);
+    } else if (!running) {
+      clearInterval(intervalId);
+    }
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [running]);
 
   return (
     <>
-      <div>
-        <span>
-          {("0"+Math.floor(time/60000)%60).slice(-2)} : 
-        </span>
-        <span>
-          {("0"+Math.floor(time/1000)%60).slice(-2)} :
-        </span>
-        <span>
-          {("0"+Math.floor(time/10)%100).slice(-2)}
-        </span>
-      </div>
-      <div>
+      <div className="max-w flex flex-col items-center justify-center py-8">
+        <h1 className="text-2xl font-semibold pb-2">STOPWATCH</h1>
+        <div className="text-xl font-semibold pb-2 ">
+          <span>{("0" + (Math.floor(time / 60000) % 60)).slice(-2)} :</span>
+          <span>{("0" + (Math.floor(time / 1000) % 60)).slice(-2)} :</span>
+          <span>{("0" + (Math.floor(time / 10) % 100)).slice(-2)}</span>
+        </div>
 
-        {
-          (!running) ? <button onClick={()=>{setRunning(true)}}>Start</button> :
-          <button onClick={()=>setRunning(false)}>Stop</button> 
-
-        }
-         <button onClick={()=>setTime(0)}>Reset</button>
-
+        <div className=" w-1/2 flex flex-row justify-evenly">
+          {!running ? (
+            <button
+              onClick={() => {
+                setRunning(true);
+              }}
+              className="border rounded-lg py-1 px-2.5"
+            >
+              Start
+            </button>
+          ) : (
+            <button
+              onClick={() => setRunning(false)}
+              className="border rounded-lg py-1 px-2.5"
+            >
+              Stop
+            </button>
+          )}
+          <button
+            onClick={() => setTime(0)}
+            className="border rounded-lg py-1 px-2.5"
+          >
+            Reset
+          </button>
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
